4487936c6dd271ed91f1adb5a0534e60000057cd
Lrlyndlcrz <delacruzlorielyn5@gmail.com>
2024-05-25 20:41:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const btnLike2 = document.getElementById("btnLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
 
-function hitLikes1() {
+function hitLikes2() {
    let totalLikes2 = parseInt(countLikes1.value) + 2
    countLikes1.textContent = totalLikes1.toString()
 }
