bd61b8ae98f3853ae78b1736fdfe0b66b4a79cda
Lrlyndlcrz <delacruzlorielyn5@gmail.com>
2024-05-25 12:44:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const btnLike1 = document.getElementById("btnLike1")
-const btnLike2 = document.getElementById("btnLike2")
+const btnLike1 = document.getElementById("btnLikes1")
+const btnLike2 = document.getElementById("btnLikes2")
 const countLike1 = document.getElementById("countLike1)
 const countLike2 = document.getElementById("countLike2)
 
