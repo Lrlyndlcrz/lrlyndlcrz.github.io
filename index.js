b995fddeec15c458a7e5aaf82c30f051f3a0333b
Lrlyndlcrz <delacruzlorielyn5@gmail.com>
2024-05-25 12:01:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btnlike1 = document.getElementById("btnLike1")
 const btnlike2 = document.getElementById("btnLike2")
-const countLikes1 = documentElementById("countLikes1)
-const countLikes2 = documentElementById("countLikes2)
+const countLikes1 = document.getElementById("countLikes1)
+const countLikes2 = document.getElementById("countLikes2)
 
 function hitLikes1() {
         let totalLikes1 = parseInt(countLikes1.value) + 1
