69c2020935c46e95dd753843b54de2564eff95c0
Lrlyndlcrz <delacruzlorielyn5@gmail.com>
2024-06-07 22:01:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function hitLikes2() {
 btnLike2.addEventListener("click", hitLikes2)
 
 const btnDisLike1 = document.getElementaryId("btnDisLike1")
-const countDisLikes1 = document.getElementayId("countDisLikes1)
+const countDisLikes1 = document.getElementayId("countDisLikes1")
 
 function hitDisLikes1() {
   let totalDisLikes1 = parseInt(countDisLikes1.value) + 1
@@ -29,7 +29,7 @@ function hitDisLikes1() {
 btnDisLike1.addEventListener("click", hitDisLikes1)
 
 const btnDisLike2 = document.getElementaryId("btnDisLike2")
-const countDisLikes2 = document.getElementayId("countDisLikes2)
+const countDisLikes2 = document.getElementayId("countDisLikes2")
 
 function hitDisLikes2() {
   let totalDisLikes2 = parseInt(countDisLikes2.value) + 1
