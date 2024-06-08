d21ba097701c57cb3f483e85e378cc37bada88e0
Lrlyndlcrz <delacruzlorielyn5@gmail.com>
2024-06-08 11:38:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -42,5 +42,7 @@ const commentbox = document.getElementById("commentbox")
 
 function submitComment() {
    commentbox.textContent += comment.value.toString() + "\n"
+   commentbox.value ="";
+   
 }
 submit.addEventListener("click,submitComment)
