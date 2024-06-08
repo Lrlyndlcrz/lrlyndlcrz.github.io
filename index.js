c4408e5a6bc908a318da53d4bfa87dc5f047c040
Lrlyndlcrz <delacruzlorielyn5@gmail.com>
2024-06-08 11:15:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const btnLike2 = document.getElementById("btnLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
 
+const btnDisLike1 = document.getElementById("btnDisLike1")
+const btnDisLike2 = document.getElementById("btnDisLike2")
+
+const countDisLikes1 = document.getElementById("countDisLikes1")
+const countDisLikes2 = document.getElementById("countDisLikes2")
+
+
 function hitLikes1() {
    let totalLikes1 = parseInt(countLikes1.value) + 1
    countLikes1.textContent = totalLikes1.toString()
@@ -16,30 +23,24 @@ function hitLikes2() {
 } 
 btnLike2.addEventListener("click",hitLikes2)
 
-const btnDisLike1 = document.getElementById("btnDisLike1")
-const btnDisLike2 = document.getElementById("btnDisLike2")
-
-const countDisLikes1 = document.getElementById("countDisLikes1")
-const countDisLikes2 = document.getElementById("countDisLikes2")
-
-
-function hitDisLikes1() {
+function DisLikes1() {
   let totalDisLikes1 = parseInt(countDisLikes1.value) + 1
-   countDiikes1.textContent = totalDisLikes1.toString()
+   countDisLikes1.textContent = totalDisLikes1.toString()
 }
 btnDisLike1.addEventListener("click",DisLikes1)
 
-function hitDisLikes2() {
+function DisLikes2() {
   let totalDisLikes2 = parseInt(countDisLikes2.value) + 1
    countDisLikes2.textContent = totalDisLikes2.toString()
 }
 btnDisLike2.addEventListener("click",DisLikes2)
 
-const submit = document.getElementById("submit")
+
 const comment = document.getElementById("comment")
+const submit = document.getElementById("submit")
 const commentbox = document.getElementById("commentbox")
 
 function submitComment() {
-   commentbox.textContent += comment.value.toString() + "\n"
+   commentbox.textContent += comment1.value.toString() + "\n"
 }
-submit.addEventListener("click,submitComment)
+submit.addEventListener("click,submitComment1)
